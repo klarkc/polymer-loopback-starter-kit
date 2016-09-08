0a2fcf3c5f66d6dc1566e4d13cb558dc479355ac
klarkc <walker@praiseweb.com.br>
2016-09-08 09:57:59 -0300

    console.log LocalTodo

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -14,7 +14,8 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
-  
+  app.lb = require('lbclient');
+
   // Sets app default base URL
   app.baseUrl = '/';
   if (window.location.port === '') {  // if production
